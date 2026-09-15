ed2cd81fffcf1bf143454accc001c7c58805cdf8
ngudt033 <ngudt033@mymail.unisa.edu.au>
2026-09-15 16:49:42 +0930

    Fixed admin uptime to fit criteria

--- Assignment1/src/main/java/comp3011/assignment1/controller/AdminController.java
+++ Assignment1/src/main/java/comp3011/assignment1/controller/AdminController.java
@@ -1,5 +1,7 @@
 package comp3011.assignment1.controller;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,15 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AdminController {
 
-    private final long startTime;
+    private final Instant startTime;
 
     public AdminController() {
-        startTime = System.currentTimeMillis();
+        startTime = Instant.now();
     }
 
     @GetMapping("/api/v1/admin/uptime")
-    public Map<String, Long> getUptime() {
-        long uptime = System.currentTimeMillis() - startTime;
-        return Map.of("uptime", uptime);
+    public Map<String, Object> getUptime() {
+
+        Instant now = Instant.now();
+
+        double uptimeSeconds =
+                Duration.between(startTime, now).toMillis() / 1000.0;
+
+        return Map.of(
+                "utcServerStart", startTime.toString(),
+                "utcNow", now.toString(),
+                "serverUptimeSeconds", uptimeSeconds);
     }
 }
